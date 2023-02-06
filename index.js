987230f2d86630802abd973fbb71bb5cab9f2699
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2023-02-06 17:30:12 -0300

    Feat: Mudança do arquivo de rotas para pasta independente e ajuste do arquivo de config da Vercel

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import routes from './src/routes/routes.js'
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({message: 'Hello from the API'})
-})
+app.use(routes)
 
 app.listen(4000, () => {
     console.log('API listening on port 4000')
